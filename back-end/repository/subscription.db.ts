a364d052444b833d0a9008a57d7bbc2c210026f4
Wiebe2004 <wiebe.delvaux@gmail.com>
2024-12-18 14:22:08 +0100

    Subscription DB half klaar

--- back-end/repository/subscription.db.ts
+++ back-end/repository/subscription.db.ts
@@ -1,27 +1,61 @@
 import { Subscription } from '../model/subscription'; // Assuming you have a Subscription model
 import orderService from '../service/order.service';
+import database from '../util/database';
 
 const subscriptions: Subscription[] = [];
 
 // Function to retrieve all subscriptions
-const getAllSubscriptions = (): Subscription[] => {
-    return subscriptions;
+const getAllSubscriptions = async (): Promise<Subscription[]> => {
+    try {
+        const subPrisma = await database.subscription.findMany();
+        return subPrisma.map((subPrisma) => Subscription.from(subPrisma))
+    } catch (error) {
+        console.error(error);
+        throw new Error("Database error. See server log for details.");
+    }
 };
 
 // Function to retrieve a subscription by ID
-const getSubscriptionById = (id: number): Subscription | null => {
-    const subscription = subscriptions.find((sub) => sub.getId() === id);
-    return subscription || null;
+const getSubscriptionById = async ({ id }: { id: number }): Promise<Subscription | null> => {
+    try {
+        const subPrisma = await database.subscription.findUnique({
+            where: { id },
+        });
+        return subPrisma ? Subscription.from(subPrisma) : null;
+    } catch (error) {
+        console.error(error)
+        throw new Error("Database error. See server log for details.");
+    }
 };
 
 // Function to save a new subscription
-const saveSubscription = (subscription: Subscription): Subscription => {
-    subscriptions.push(subscription); // Save the Subscription instance directly
-    return subscription;
+const saveSubscription = async (subscription: Subscription): Promise<Subscription> => {
+    const subPrisma = await database.subscription.create({
+        data: {
+            id: subscription.getId(),
+            region: subscription.getRegion(),
+            subtype: subscription.getSubtype(),
+            startDate: subscription.getStartDate(), 
+            endDate: subscription.getEndDate(),
+            orderId: subscription.getOrderId(),
+        },
+    });
+    return Subscription.from(subPrisma);
 };
 
-const getSubscriptionByReferentie = (orderReferentie: string) => {
-    return subscriptions.find(subscription => subscription.getOrderId() === orderReferentie);
+const getSubscriptionByReferentie = async (orderReferentie: string) => {
+    try {
+        const subscription = await database.subscription.findFirst({
+            where: {
+                orderId: orderReferentie,
+            },
+        });
+
+        return subscription ? Subscription.from(subscription) : null; 
+    } catch (error) {
+        console.error(error);
+        throw new Error("Database error. See server log for details.");
+    }
 };
 
 const findSubscriptionsByUserId = async (userId: string): Promise<Subscription[]> => {
